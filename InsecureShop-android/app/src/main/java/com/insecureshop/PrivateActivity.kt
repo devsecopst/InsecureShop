11d808bd2400bcd1606b9b523ed3b677e0813c2d
devsecopst <devsecopst@gmail.com>
2025-07-06 14:39:00 +0300

    test

--- InsecureShop-android/app/src/main/java/com/insecureshop/PrivateActivity.kt
+++ InsecureShop-android/app/src/main/java/com/insecureshop/PrivateActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_product_list.*
 class PrivateActivity : AppCompatActivity() {
 
     val USER_AGENT =
-        "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/537.46 (KHTML, like Gecko) Chrome/43.0.2357.65 Mobile Safari/537.36"
+        "Mozilla/5.0 (Linux; Android 6.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/537.46 (KHTML, like Gecko) Chrome/43.0.2357.65 Mobile Safari/537.36"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
